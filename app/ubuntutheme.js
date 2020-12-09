e73f44530d2d33585b34de1215c41eb5fc5f258d
David Thiele <david@sufftrupp.de>
2020-12-09 22:04:09 +0100

    Initial Commit

---
+++ app/ubuntutheme.js
@@ -0,0 +1,66 @@
+(window.onload = function () {
+	var css = 
+	"@media (max-width: 990px){\
+		body{\
+			overflow:auto !important;\
+			min-width: unset !important;\
+		}\
+		#page_topbar{\
+			min-width: unset !important;\
+		} \
+		#page_sidebar{\
+			visibility:hidden !important;\
+		} \
+		#page_topbar{\
+			left: 0px !important;\
+		}\
+		#page_content{\
+			margin-left: 0px !important;\
+		}\
+		.player-track{\
+			padding: 0px !important;\
+		}\
+		#page_player .player-bottom{\
+			min-width: 0px!important;\
+			overflow-x: scroll !important;\
+			height: auto;\
+			display: block !important;\
+		} \
+		.page-player .player-bottom .player-track .track-container .track-heading .track-actions{\
+			display: none;\
+		} \
+		.page-player .player-bottom .player-track{\
+			width: 100%;\
+		}\
+		.page-player .player-bottom .player-controls{\
+			width: 100%;\
+		}\
+		.track-title{\
+			min-width: 80px !important;\
+		}\
+		.page-player .player-queuelist .player-container {\
+			margin: 12px 2% 0;\
+		}\
+		.page-player .player-full .player-container {\
+			display: block;\
+			overflow: scroll;\
+		}\
+	}";
+
+	if (typeof GM_addStyle != "undefined") {
+		GM_addStyle(css);
+	} else if (typeof PRO_addStyle != "undefined") {
+		PRO_addStyle(css);
+	} else if (typeof addStyle != "undefined") {
+		addStyle(css);
+	} else {
+			var node = document.createElement("style");
+			node.appendChild(document.createTextNode(css));
+			var head = document.head;
+			if (head) {
+				head.appendChild(node);
+			} else {
+				document.documentElement.appendChild(node);
+			}
+	}
+})();
